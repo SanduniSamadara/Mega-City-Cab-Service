f0b0a73c2781c6657d4e70d9a2ead4b900e08bd1
SanduniSamadara <121673809+SanduniSamadara@users.noreply.github.com>
2025-03-01 14:11:05 +0530

    Defined CRUD methods for booking operations

---
+++ src/main/java/com/megacity/dao/BookingDAO.java
@@ -0,0 +1,12 @@
+package com.megacity.dao;
+
+import com.megacity.model.Booking;
+import java.util.List;
+
+public interface BookingDAO {
+    void addBooking(Booking booking) throws Exception;
+    Booking getBookingByNumber(String bookingNumber) throws Exception;
+    List<Booking> getAllBookings() throws Exception;
+    void updateBooking(Booking booking) throws Exception;
+    void deleteBooking(String bookingNumber) throws Exception;
+}
