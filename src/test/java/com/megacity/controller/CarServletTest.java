dca7ea9d0a497b2796347ac92674e84a6cb532cb
SanduniSamadara <121673809+SanduniSamadara@users.noreply.github.com>
2025-03-12 06:03:07 +0530

    Create CarServlet test class

---
+++ src/test/java/com/megacity/controller/CarServletTest.java
@@ -0,0 +1,13 @@
+package com.megacity.controller;
+
+import junit.framework.TestCase;
+public class CarServletTest extends TestCase {
+
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
+    public void tearDown() throws Exception {
+        super.tearDown();
+    }
+}
